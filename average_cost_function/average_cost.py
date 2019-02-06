538d2d867b321b03eb4c84f0dd74d61954ff6d78
Lukas Elflein <elfleinl@simnetpc80.imtek.privat>
2019-02-06 14:59:48 +0100

    bare bones intial files

---
+++ average_cost_function/average_cost.py
@@ -0,0 +1,10 @@
+""" Average Cost Functions for Horton to determine Charges for Molecular Dynamics 
+Copyright 2019 Simulation Lab
+University of Freiburg
+Author: Lukas Elflein <elfleinl@cs.uni-freiburg.de>
+"""
+
+import numpy as np
+import h5py
+
+files = []
